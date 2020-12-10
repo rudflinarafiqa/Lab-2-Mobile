e98199cd17ac41ef0c2b2df8eec577cd35c98a72
rudflinarafiqa <rafiqarudflina@gmail.com>
2020-12-10 22:17:17 +0800

    commit

---
+++ lib/loginscreen.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:meow/registerscreen.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:toast/toast.dart';
+import 'package:http/http.dart' as http;
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emcontroller = TextEditingController();
+  String _email = "";
+  final TextEditingController _pscontroller = TextEditingController();
+  String _password = "";
+  bool _rememberMe = false;
+  SharedPreferences prefs;
+
+  @override
+  void initState() {
+    loadpref();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: _onBackPressAppBar,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('LOGIN'),
+            ),
+            //resizeToAvoidBottomPadding: false,
+            body: new Container(
+              padding: EdgeInsets.all(30.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/meow1.jpg',
+                      scale: 2,
+                    ),
+                    TextField(
+                        controller: _emcontroller,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: 'Email', icon: Icon(Icons.email))),
+                    TextField(
+                      controller: _pscontroller,
+                      decoration: InputDecoration(
+                          labelText: 'Password', icon: Icon(Icons.lock)),
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                      minWidth: 300,
+                      height: 50,
+                      child: Text('Login'),
+                      color: Colors.orange[700],
+                      textColor: Colors.black,
+                      elevation: 15,
+                      onPressed: _onLogin,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Checkbox(
+                          value: _rememberMe,
+                          onChanged: (bool value) {
+                            _onChange(value);
+                          },
+                        ),
+                        Text('Remember Me', style: TextStyle(fontSize: 16))
+                      ],
+                    ),
+                    Padding(
+            padding: EdgeInsets.all(5),
+            child: RaisedButton(
+            child: Text("Register My Account", style: new TextStyle(
+                        fontWeight: FontWeight.bold),),
+            color: Colors.orange[300],
+            onPressed: _onRegister,
+           ),
+         ),
+         Padding(
+            padding: EdgeInsets.all(5),
+            child: RaisedButton(
+            child: Text("Forgot Account" , style: new TextStyle(
+                        fontWeight: FontWeight.bold),),
+            color: Colors.orange[300],
+            onPressed: _onForgot,
+           ),
+         ),
+                  ],
+                ),
+              ),
+            )));
+  }
+
+  Future<void> _onLogin() async {
+    _email = _emcontroller.text;
+    _password = _pscontroller.text;
+     ProgressDialog pr = new ProgressDialog(context,
+        type: ProgressDialogType.Normal, isDismissible: false);
+    pr.style(message: "Login...");
+    await pr.show();
+    http.post("https://seriouslaa.com/meow/php/login_user.php", body: {
+      "email": _email,
+      "password": _password,
+    }).then((res) {
+      print(res.body);
+      if (res.body == "success") {
+        Toast.show(
+          "Login Succes",
+          context,
+          duration: Toast.LENGTH_LONG,
+          gravity: Toast.TOP,
+        );
+        Navigator.push(context,
+            MaterialPageRoute(builder: (BuildContext context) => RegisterScreen()));
+      } else {
+        Toast.show(
+          "Login failed",
+          context,
+          duration: Toast.LENGTH_LONG,
+          gravity: Toast.TOP,
+        );
+      }
+    }).catchError((err) {
+      print(err);
+    });
+    await pr.hide();
+  }
+
+  void _onRegister() {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) => RegisterScreen()));
+  }
+
+  void _onForgot() {
+    print('Forgot');
+  }
+
+  void _onChange(bool value) {
+    setState(() {
+      _rememberMe = value;
+      savepref(value);
+    });
+  }
+
+  void loadpref() async {
+    prefs = await SharedPreferences.getInstance();
+    _email = (prefs.getString('email')) ?? '';
+    _password = (prefs.getString('password')) ?? '';
+    _rememberMe = (prefs.getBool('rememberme')) ?? false;
+    if (_email.isNotEmpty) {
+      setState(() {
+        _emcontroller.text = _email;
+        _pscontroller.text = _password;
+        _rememberMe = _rememberMe;
+      });
+    }
+  }
+
+  void savepref(bool value) async {
+    prefs = await SharedPreferences.getInstance();
+    _email = _emcontroller.text;
+    _password = _pscontroller.text;
+
+    if (value) {
+      if (_email.length < 5 && _password.length < 3) {
+        print("EMAIL/PASSWORD EMPTY");
+        _rememberMe = false;
+        Toast.show(
+          "Email/password empty!",
+          context,
+          duration: Toast.LENGTH_LONG,
+          gravity: Toast.BOTTOM,
+        );
+        return;
+      } else {
+        await prefs.setString('email', _email);
+        await prefs.setString('password', _password);
+        await prefs.setBool('rememberme', value);
+        Toast.show(
+          "Preferences saved",
+          context,
+          duration: Toast.LENGTH_LONG,
+          gravity: Toast.BOTTOM,
+        );
+        print("SUCCESS");
+      }
+    } else {
+      await prefs.setString('email', '');
+      await prefs.setString('password', '');
+      await prefs.setBool('rememberme', false);
+      setState(() {
+        _emcontroller.text = "";
+        _pscontroller.text = "";
+        _rememberMe = false;
+      });
+      Toast.show(
+        "Preferences removed",
+        context,
+        duration: Toast.LENGTH_LONG,
+        gravity: Toast.BOTTOM,
+      );
+    }
+  }
+
+  Future<bool> _onBackPressAppBar() async {
+    SystemNavigator.pop();
+    print('Backpress');
+    return Future.value(false);
+  }
+}
---
+++ lib/registerscreen.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:meow/loginscreen.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:toast/toast.dart';
+import 'package:http/http.dart' as http;
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _namecontroller = TextEditingController();
+  final TextEditingController _emcontroller = TextEditingController();
+  final TextEditingController _pscontroller = TextEditingController();
+  final TextEditingController _phcontroller = TextEditingController();
+
+  String _email = "";
+  String _password = "";
+  String _name = "";
+  String _phone = "";
+  bool _passwordVisible = false;
+  bool _rememberMe = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('REGISTRATION'),
+      ),
+      body: Container(
+          child: Padding(
+              padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/meow.gif',
+                      scale: 2,
+                    ),
+                    TextField(
+                        controller: _namecontroller,
+                        keyboardType: TextInputType.name,
+                        decoration: InputDecoration(
+                            labelText: 'Name', icon: Icon(Icons.person))),
+                    TextField(
+                        controller: _emcontroller,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: 'Email', icon: Icon(Icons.email))),
+                    TextField(
+                        controller: _phcontroller,
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                            labelText: 'Mobile No', icon: Icon(Icons.phone))),
+                    TextField(
+                      controller: _pscontroller,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        icon: Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _passwordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Theme.of(context).primaryColorDark,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _passwordVisible = !_passwordVisible;
+                            });
+                          },
+                        ),
+                      ),
+                      obscureText: _passwordVisible,
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: <Widget>[
+                        Checkbox(
+                          value: _rememberMe,
+                          onChanged: (bool value) {
+                            _onChange(value);
+                          },
+                        ),
+                        Text('Remember Me', style: TextStyle(fontSize: 16))
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                      minWidth: 300,
+                      height: 50,
+                      child: Text('Register'),
+                      color: Colors.orange[700],
+                      textColor: Colors.black,
+                      elevation: 15,
+                      onPressed: _onRegister,
+                    ),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                        onTap: _onLogin,
+                        child: Text('Already register',
+                            style: TextStyle(fontSize: 16))),
+                  ],
+                ),
+              ))),
+    );
+  }
+
+  void _onRegister() async {
+    _name = _namecontroller.text;
+    _email = _emcontroller.text;
+    _password = _pscontroller.text;
+    _phone = _phcontroller.text;
+    if (validateEmail(_email) && validatePassword(_password)) {
+       Toast.show(
+          "Check your email/password",
+          context,
+          duration: Toast.LENGTH_LONG,
+          gravity: Toast.TOP,
+        );
+      return;
+    }
+    ProgressDialog pr = new ProgressDialog(context,
+        type: ProgressDialogType.Normal, isDismissible: false);
+    pr.style(message: "Registration...");
+    await pr.show();
+    http.post("https://seriouslaa.com/meow/php/register_user.php",
+        body: {
+          "name": _name,
+          "email": _email,
+          "password": _password,
+          "phone": _phone,
+        }).then((res) {
+      print(res.body);
+      if (res.body == "succes") {
+        Toast.show(
+          "Registration success. An email has been sent to .$_email. Please check your email for OTP verification. Also check in your spam folder.",
+          context,
+          duration: Toast.LENGTH_LONG,
+          gravity: Toast.TOP,
+        );
+        if (_rememberMe) {
+          savepref();
+        }
+       _onLogin();
+      } else {
+        Toast.show(
+          "Registration failed",
+          context,
+          duration: Toast.LENGTH_LONG,
+          gravity: Toast.TOP,
+        );
+      }
+    }).catchError((err) {
+      print(err);
+    });
+    await pr.hide();
+  }
+
+  void _onLogin() {
+    Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (BuildContext context) => LoginScreen()));
+  }
+
+  void _onChange(bool value) {
+    setState(() {
+      _rememberMe = value;
+    });
+  }
+
+  void savepref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _email = _emcontroller.text;
+    _password = _pscontroller.text;
+    await prefs.setString('email', _email);
+    await prefs.setString('password', _password);
+    await prefs.setBool('rememberme', true);
+  }
+
+  bool validateEmail(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    return (!regex.hasMatch(value)) ? false : true;
+  }
+
+  bool validatePassword(String value){
+        String  pattern = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$';
+        RegExp regExp = new RegExp(pattern);
+        return regExp.hasMatch(value);
+  }
+}
